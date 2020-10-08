4d85ceb57bdc71537680495a61c370a7f2bd5f9f
Michal Zygor <michal@winami.com>
2020-10-08 19:06:03 +0200

    dodanie trzeciego towaru

--- src/Shop.java
+++ src/Shop.java
@@ -12,16 +12,32 @@ public class Shop {
         product2.availability = 1000;
         product2.amount = 1.13;
 
+        Product product3 = new Product();
+        product3.name = "sok jabłkowy";
+        product3.manufacturer = "Helena";
+        product3.availability = 500;
+        product3.amount = 4.59;
+
         System.out.println("nazwa: " + product1.name
             + ", producent: " + product1.manufacturer
             + ", cena: " + product1.amount
             + ", dostępność: " + product1.availability);
+
+
+        System.out.println("nazwa: " + product3.name
+                + ", producent: " + product3.manufacturer
+                + ", cena: " + product3.amount
+                + ", dostępność: " + product3.availability);
 
         System.out.println("nazwa: " + product2.name
                 + ", producent: " + product2.manufacturer
                 + ", cena: " + product2.amount
                 + ", dostępność: " + product2.availability);
 
-    }
 
+    }
 }
+
+
+
+
